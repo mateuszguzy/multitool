0e5ddac139a6672fd40ff0f12b932a4f82a07fd7
Mateusz Guzy <mateusz_guzy@rapid7.com>
2023-05-08 17:44:35 +0200

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+
+from utils.utils import create_steering_module_instance_with_user_input
+
+USER_INPUT_MOCK_SINGLE_MODULE_1 = (
+    "tests/mocked_user_input/user_input_mock_single_module_1.json"
+)
+USER_INPUT_MOCK_SINGLE_MODULE_2 = (
+    "tests/mocked_user_input/user_input_mock_single_module_2.json"
+)
+USER_INPUT_MOCK_SINGLE_MODULE_3 = (
+    "tests/mocked_user_input/user_input_mock_single_module_3.json"
+)
+USER_INPUT_MOCK_SINGLE_PHASE_1 = (
+    "tests/mocked_user_input/user_input_mock_single_phase_1.json"
+)
+USER_INPUT_MOCK_SINGLE_PHASE_2 = (
+    "tests/mocked_user_input/user_input_mock_single_phase_2.json"
+)
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        USER_INPUT_MOCK_SINGLE_MODULE_1,
+        USER_INPUT_MOCK_SINGLE_MODULE_2,
+        USER_INPUT_MOCK_SINGLE_MODULE_3,
+    ],
+)
+def test_input_1(request):
+    return create_steering_module_instance_with_user_input(request.param)
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        USER_INPUT_MOCK_SINGLE_PHASE_1,
+        USER_INPUT_MOCK_SINGLE_PHASE_2,
+    ],
+)
+def test_input_2(request):
+    return create_steering_module_instance_with_user_input(request.param)
