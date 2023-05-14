856ce27f75f8a39928d3691375d7201291e36894
Mateusz Guzy <mateusz_guzy@rapid7.com>
2023-05-14 13:51:44 +0200

    create Request Manager class

---
+++ modules/network/request_manager.py
@@ -0,0 +1,42 @@
+import logging
+from typing import ContextManager
+
+import requests
+from pydantic import BaseModel
+from requests.exceptions import ConnectionError
+
+from utils.abstracts_classes import Module
+
+
+class RequestManager(ContextManager, Module, BaseModel):
+    method: str = str()
+    url: str = str()
+    session: requests.Session = object()
+
+    # 'pydantic' required class - provide additional config to allow 'requests.Session' type check
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __init__(self, method: str, url: str) -> None:
+        super().__init__()
+        self.method = method
+        self.url = url
+        self.session = requests.Session()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+    def run(self):
+        return self._make_request(method=self.method, url=self.url)
+
+    def _make_request(self, method: str, url: str):
+        if method.lower() == "get":
+            try:
+                return self.session.get(url=url, allow_redirects=False)
+            except ConnectionError as e:
+                logging.error(f"Connection Error: {e}")
+                # TODO: change app behaviour after ConnectionError
+                exit()
