65ff59fcbf9a11dd2b669d49aa9e66987a1ca7b1
Mateusz Guzy <mateusz_guzy@rapid7.com>
2023-05-14 13:25:42 +0200

    change settings file directory

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # --- DIRECTORIES
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 WORDLISTS_DIR = "/utils/wordlists"
 
 # --- STEERING MODULE
