a0782d5d51ddd35384427bbe0c3619e9f2cec590
Mateusz Guzy <mateusz.guzy@stxnext.com>
2023-07-18 14:19:49 +0200

    fix tests failed imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,24 @@
 import pytest
 
+from config.settings import TESTS_MOCKED_INPUT_DIR, BASE_DIR
 from modules.network.request_manager.request_manager import RequestManager
 from utils.utils import create_steering_module_instance_with_user_input
 
+
 USER_INPUT_MOCK_SINGLE_MODULE_1 = (
-    "tests/mocked_user_input/user_input_mock_single_module_1.json"
+    f"{TESTS_MOCKED_INPUT_DIR}/user_input_mock_single_module_1.json"
 )
 USER_INPUT_MOCK_SINGLE_MODULE_2 = (
-    "tests/mocked_user_input/user_input_mock_single_module_2.json"
+    f"{TESTS_MOCKED_INPUT_DIR}/user_input_mock_single_module_2.json"
 )
 USER_INPUT_MOCK_SINGLE_MODULE_3 = (
-    "tests/mocked_user_input/user_input_mock_single_module_3.json"
+    f"{TESTS_MOCKED_INPUT_DIR}/user_input_mock_single_module_3.json"
 )
 USER_INPUT_MOCK_SINGLE_PHASE_1 = (
-    "tests/mocked_user_input/user_input_mock_single_phase_1.json"
+    f"{TESTS_MOCKED_INPUT_DIR}/user_input_mock_single_phase_1.json"
 )
 USER_INPUT_MOCK_SINGLE_PHASE_2 = (
-    "tests/mocked_user_input/user_input_mock_single_phase_2.json"
+    f"{TESTS_MOCKED_INPUT_DIR}/user_input_mock_single_phase_2.json"
 )
 USER_INPUT_MOCK_RUN_ALL_1 = "tests/mocked_user_input/user_input_mock_run_all_1.json"
 
