4f0b346abc1fcd3373d6ec452020e8166f3c2a9b
Mateusz Guzy <mateusz.guzy@stxnext.com>
2023-07-19 20:47:26 +0200

    wrap test function in test class

--- tests/integration/modules/core/test_steering_module.py
+++ tests/integration/modules/core/test_steering_module.py
@@ -1,11 +1,14 @@
 from modules.helper.redis_client import RedisClient
 
 
-def test_run_directory_bruteforce_module(integration_steering_module_with_directory_bruteforce_test_input):
-    integration_steering_module_with_directory_bruteforce_test_input.run()
+class TestSteeringModule:
+    def test_run_directory_bruteforce_module(
+        self, integration_steering_module_with_directory_bruteforce_test_input
+    ):
+        integration_steering_module_with_directory_bruteforce_test_input.run()
 
-    with RedisClient() as rc:
-        keys = rc.keys("directory_bruteforce_*:")
-        result = (rc.mget(keys))
+        with RedisClient() as rc:
+            keys = rc.keys("directory_bruteforce_*:")
+            result = rc.mget(keys)
 
-    assert result == [b'vulnerabilities']
+        assert result == [b"vulnerabilities"]
