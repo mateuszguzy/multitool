9ce9c1dceb26b4caf2573591705db666fe072034
Mateusz Guzy <mateusz.guzy@stxnext.com>
2023-07-26 22:01:03 +0200

    fix custom exceptions behaviour

--- utils/custom_exceptions.py
+++ utils/custom_exceptions.py
@@ -1,24 +1,14 @@
 class CustomConnectionError(Exception):
-    # TODO: why is message added twice every time? Multithreading?
-    def __init__(self, exc: str, message: str = ""):
-        message = exc.split(" (")[0]
-        super().__init__(message)
+    pass
 
 
 class InvalidUrl(Exception):
-    def __init__(self, exc: str, message: str = ""):
-        message = exc.split(":")[0]
-        super().__init__(message)
+    pass
 
 
 class UnhandledRequestMethod(Exception):
-    def __init__(
-        self, method: str, message="Unhandled request method used: %s. Shutting down."
-    ):
-        message = message % method
-        super().__init__(message)
+    pass
 
 
 class UnhandledException(Exception):
-    def __init__(self, message="Unexpected error occurred. Shutting down."):
-        super().__init__(message)
+    pass
