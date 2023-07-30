f24f4fd0a1e7630216a68d5585699dad10fdf230
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-07-30 17:54:12 +0200

    add: new test cases for CLI testing

--- tests/unit/modules/user_interface/cli/test_cli_interface.py
+++ tests/unit/modules/user_interface/cli/test_cli_interface.py
@@ -46,3 +46,109 @@ class TestCliInterface:
         result = clean_and_validate_input_targets(test_input)
 
         assert len(result) == expected
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_click_prompt")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("a", "all"),
+            ("A", "all"),
+            ("sm", "single_module"),
+            ("SM", "single_module"),
+            ("sp", "single_phase"),
+            ("SP", "single_phase"),
+        ],
+    )
+    def test_use_type_question_success(self, test_input, expect, cli_interface):
+        cli_interface.use_type_question()
+
+        assert cli_interface.use_type == expect
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_click_prompt")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("r", "recon"),
+            ("R", "recon"),
+        ],
+    )
+    def test_phase_question_success(self, test_input, expect, cli_interface):
+        cli_interface.phase_question()
+
+        assert cli_interface.phase == expect
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_click_prompt")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("dirb", "directory_bruteforce"),
+            ("DIRB", "directory_bruteforce"),
+        ],
+    )
+    def test_module_question_success(self, test_input, expect, cli_interface):
+        cli_interface.module_question()
+
+        assert cli_interface.module == expect
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_click_prompt")
+    @pytest.mark.usefixtures("mock_clean_and_validate_input_targets")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("https://www.example.com:8080", "https://www.example.com:8080/"),
+            ("https://www.example.com", "https://www.example.com/"),
+            ("https://example.com", "https://example.com/"),
+            ("https://example:8080", "https://example:8080/"),
+            ("http://example.com", "http://example.com/"),
+            ("http://example:8080", "http://example:8080/"),
+            ("www.example.com", "http://www.example.com/"),
+            ("http://example:8080, www.example.com", "http://example:8080/,http://www.example.com/"),
+
+        ],
+    )
+    def test_targets_question_success(self, test_input, expect, cli_interface):
+        cli_interface.targets_question()
+
+        assert cli_interface.targets == expect.split(",")
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_click_prompt")
+    @pytest.mark.usefixtures("mock_clean_and_validate_input_targets")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("https://www.example.com:8080", "https://www.example.com:8080/"),
+            ("https://www.example.com", "https://www.example.com/"),
+            ("https://example.com", "https://example.com/"),
+            ("https://example:8080", "https://example:8080/"),
+            ("http://example.com", "http://example.com/"),
+            ("http://example:8080", "http://example:8080/"),
+            ("www.example.com", "http://www.example.com/"),
+            ("http://example:8080, www.example.com", "http://example:8080/,http://www.example.com/"),
+
+        ],
+    )
+    def test_targets_helper_question_success(self, test_input, expect, cli_interface):
+        cli_interface.targets_helper_question()
+
+        assert cli_interface.targets == expect.split(",")
+
+    @pytest.mark.usefixtures("cli_interface")
+    @pytest.mark.usefixtures("mock_directory_bruteforce_questions")
+    @pytest.mark.parametrize(
+        "test_input, expect",
+        [
+            ("small", {"list_size": "small"}),
+            ("medium", {"list_size": "medium"}),
+            ("large", {"list_size": "large"}),
+        ],
+    )
+    def test_recon_phase_questions_success(self, test_input, expect, cli_interface):
+        cli_interface.directory_bruteforce_input = expect
+        cli_interface.recon_phase_questions()
+
+        assert cli_interface.recon_phase_input == {"directory_bruteforce": expect}
