faccd12f5bc2244b09a65b9ddd25f082c508dbb2
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-09-21 19:17:36 +0200

    rework: - main function exception handling - change traceback setting to env var

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sys
 
-import click
 from click import Abort
 
-from config.settings import SHOW_TRACEBACKS
+from config.settings import steering_module_logger, SHOW_TRACEBACKS
 from modules.core.steering_module.steering_module import SteeringModule
-from modules.task_queue.tasks import log_results
 from modules.user_interface.user_interface import UserInterface
-from utils.utils import prepare_final_results_dictionary
+from utils.custom_exceptions import (
+    AbortException,
+    UnhandledException,
+)
+from utils.utils import (
+    prepare_final_results_dictionary,
+    format_exception_with_traceback_for_logging,
+)
+from modules.task_queue.tasks import log_results
 
 
 def main():
@@ -16,11 +22,19 @@ def main():
 
     try:
         user_interface.run()
-    except Abort:
-        click.echo("\n\nKeyboard interrupted. Quitting.")
-        sys.exit()
+    except Abort as exc:
+        steering_module_logger.exception(
+            format_exception_with_traceback_for_logging(exc)
+        )
+        raise AbortException("Program aborted")
+    except Exception as exc:
+        steering_module_logger.exception(
+            format_exception_with_traceback_for_logging(exc)
+        )
+        raise UnhandledException("Unhandled exception")
 
-    steering_module = SteeringModule(user_interface.cli_interface.user_input)
+    user_input = getattr(user_interface.cli_interface, "user_input")
+    steering_module = SteeringModule(user_input=user_input)
     steering_module.run()
 
     results = prepare_final_results_dictionary()
