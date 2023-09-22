3d44f324ea9d871b7eb9d3cdad2e66734d2687dd
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-09-23 00:38:57 +0200

    move cli_interface to instance attribute

--- modules/user_interface/user_interface.py
+++ modules/user_interface/user_interface.py
@@ -3,10 +3,9 @@ from utils.abstracts_classes import AbstractModule
 
 
 class UserInterface(AbstractModule):
-    cli_interface: CliInterface = CliInterface()
-
     def __init__(self):
         super().__init__()
+        self.cli_interface = CliInterface()
 
     def run(self):
         self.cli_interface.run()
