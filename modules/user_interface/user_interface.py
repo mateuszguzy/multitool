f7789a610e04c1669aa67c2ea37b01ea7c7e9f85
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-09-24 00:24:34 +0200

    fix bug with high level class not returning result

--- modules/user_interface/user_interface.py
+++ modules/user_interface/user_interface.py
@@ -5,7 +5,7 @@ from utils.abstracts_classes import AbstractModule
 class UserInterface(AbstractModule):
     def __init__(self):
         super().__init__()
-        self.cli_interface = CliInterface()
 
     def run(self):
-        self.cli_interface.run()
+        cli_interface = CliInterface()
+        return cli_interface.run()
