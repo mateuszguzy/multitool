8d63895fde314002c07fa464f60d45ad0552f97f
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-09-23 11:54:26 +0200

    minor celery tasks rework: - change 'id-else' to dictionary mapping - change async task launch function - change string concat to 'urljoin'

--- modules/task_queue/tasks.py
+++ modules/task_queue/tasks.py
@@ -1,26 +1,44 @@
+from typing import Optional
+from urllib.parse import urljoin
+
 from config.settings import directory_bruteforce_logger, steering_module_logger
 from modules.task_queue.celery import app
 from modules.network.request_manager.request_manager import RequestManager
 
 
+# Constants for module names
+STEERING_MODULE = "__main__"
+DIRECTORY_BRUTEFORCE_MODULE = "directory_bruteforce"
+
+# Dictionary mapping module names to loggers
+loggers = {
+    STEERING_MODULE: steering_module_logger,
+    DIRECTORY_BRUTEFORCE_MODULE: directory_bruteforce_logger
+}
+
+
 @app.task
 def log_results(result: str, module: str, target: str = "") -> None:
+    """
+    Logs the result using the appropriate logger based on the module name.
+    If a target is provided, it is included in the log message.
+    """
     if target:
         result = f"<{target}>: {result}"
 
-    if "__main__" in module:
-        steering_module_logger.info(result)
-    elif "directory_bruteforce" in module:
-        directory_bruteforce_logger.info(result)
+    if module in loggers:
+        loggers[module].info(result)
+    else:
+        loggers[STEERING_MODULE].error(f"Wrong type of module used. Result: {result}")
 
 
 @app.task
-def web_request(request_method: str, url: str, word: str, module: str) -> str or None:  # type: ignore
-    with RequestManager(method=request_method, url=f"{url}{word}") as rm:
+def web_request(request_method: str, url: str, word: str, module: str) -> Optional[str]:
+    with RequestManager(method=request_method, url=urljoin(url, word)) as rm:
         response = rm.run()
 
-        if response.status_code not in [404]:
-            log_results.delay(result=word, module=module, target=url)
+        if response.ok:
+            log_results.apply_async(args=[word, module], kwargs={'target': url})
             return word
         else:
-            return None
+            return  # type: ignore
