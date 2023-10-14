f5571cfc2186ddd03b9231388692f9855d15f8b0
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-10-14 19:56:01 +0200

    major directory bruteforce rework: - add optional recursive behaviour - currently option is always off - class reorganization - minor code fixes

--- modules/recon/directory_bruteforce/directory_bruteforce.py
+++ modules/recon/directory_bruteforce/directory_bruteforce.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import Set, List, Optional
 
 import celery  # type: ignore
 
@@ -17,39 +17,60 @@ class DirectoryBruteforce(AbstractModule):
     file_path: str = str()
     allow_redirects: bool = False
 
-    def __init__(self, directory_bruteforce_input: DirectoryBruteforceInput, target: str) -> None:
+    def __init__(
+        self, directory_bruteforce_input: DirectoryBruteforceInput, target: str
+    ) -> None:
         super().__init__()
         self.target: str = target
-        list_size = getattr(directory_bruteforce_input, "list_size")
+        list_size = getattr(directory_bruteforce_input, "list_size", "small")
+        self.recursive = getattr(directory_bruteforce_input, "recursive", False)
         self.file_path: str = f"{WORDLISTS_DIR}/dir_bruteforce_{list_size}.txt"
         self.wordlist: Set[str] = set()
+        self.directories_to_check_recursively: Set[str] = set()
+        self.final_results: Set[str] = set()
 
-    def run(self):
-        self._run_with_celery()
+    def run(self) -> None:
+        self._read_wordlist()
+        results = self._run_with_celery()
+        self.final_results.update(results)
 
-    def _run_with_celery(self):
+        if self.recursive:
+            self.directories_to_check_recursively = set(results)
+            self._run_recursively()
+
+        self.save_results(self.final_results)
+
+    def _run_with_celery(self, path: Optional[str] = None) -> List[str]:
         """
         Runs celery tasks in parallel.
         """
-        self._read_wordlist()
-
         tasks = [
             directory_bruteforce_web_request.s(
                 request_method=self.request_method,
                 target=self.target,
-                path=word,
+                path=self.concatenate_path(path, word),
                 module=__name__,
                 allow_redirects=self.allow_redirects,
             )
             for word in self.wordlist
         ]
 
-        results = convert_list_or_set_to_dict(
-            list_of_items=celery.group(tasks).apply_async().join()
-        )
-        store_module_results_in_database(
-            target=self.target, results=results, phase="recon", module="directory_bruteforce"
-        )
+        return [
+            result
+            for result in celery.group(tasks).apply_async().join()
+            if result is not None
+        ]
+
+    def _run_recursively(self) -> None:
+        """
+        This uses 'run_directory_bruteforce' method to run directory bruteforce module recursively.
+        If there are any results, it will run the same module again with the same wordlist. But this time,
+        it will use the results from previous run to create new path.
+        """
+        while self.directories_to_check_recursively:
+            results = self._run_with_celery(path=self.directories_to_check_recursively.pop())
+            self.directories_to_check_recursively.update(set(results))
+            self.final_results.update(results)
 
     def _read_wordlist(self) -> None:
         """
@@ -58,3 +79,22 @@ class DirectoryBruteforce(AbstractModule):
         with open(self.file_path, "r", encoding="utf-8") as wordlist:
             for line in wordlist:
                 self.wordlist.add(line.strip())
+
+    def save_results(self, results) -> None:
+        results = convert_list_or_set_to_dict(list_of_items=results)
+        store_module_results_in_database(
+            target=self.target,
+            results=results,
+            phase="recon",
+            module="directory_bruteforce",
+        )
+
+    @staticmethod
+    def concatenate_path(path: Optional[str], word: str) -> str:
+        """
+        Concatenates path with word. Helpful in case of recursive directory bruteforce.
+        """
+        if path:
+            return f"{path}/{word}"
+        else:
+            return word
