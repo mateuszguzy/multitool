f93467324f0610aba9228afeaa5383ddbc863cd5
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-11-11 01:37:39 +0100

    add: new test class; remove global parametrize func

--- tests/unit/modules/task_queue/test_tasks.py
+++ tests/unit/modules/task_queue/test_tasks.py
@@ -1,17 +1,18 @@
 import uuid
+from unittest import mock
 
 import pytest
 
-from modules.task_queue.tasks import log_results
+from config.settings import PUBSUB_RESULTS_CHANNEL_NAME
+from modules.task_queue.tasks import log_results, pass_result_event
 from utils.custom_dataclasses import ResultEvent
-
-pytestmark = pytest.mark.parametrize(
-    "test_target, test_module, test_result",
-    [("target_1", "module_1", "result_1"), ("target_2", "module_2", "result_2")],
-)
 
 
 class TestLogResults:
+    @pytest.mark.parametrize(
+        "test_target, test_module, test_result",
+        [("target_1", "module_1", "result_1"), ("target_2", "module_2", "result_2")],
+    )
     def test_log_results_success(
         self, test_target, test_result, test_module, mock_get_logger_function
     ):
@@ -29,6 +30,10 @@ class TestLogResults:
             f"<{test_target}>: {test_result}"
         )
 
+    @pytest.mark.parametrize(
+        "test_target, test_module, test_result",
+        [("target_1", "module_1", "result_1"), ("target_2", "module_2", "result_2")],
+    )
     def test_log_results_fail(
         self,
         test_target,
@@ -51,3 +56,61 @@ class TestLogResults:
         )
         mock_get_logger_function_with_exception.side_effect = Exception
         mock_task_queue_logger_in_tasks.exception.assert_called_once()
+
+
+class TestPassResultEvent:
+    task_name = "pass_result_event"
+
+    @pytest.mark.parametrize(
+        "encoded_event",
+        ["encoded_event_1", "encoded_event_2"],
+    )
+    def test_pass_result_event_success(
+        self,
+        encoded_event,
+        mocker,
+        mock_task_queue_logger_in_tasks,
+        mock_redis_in_tasks,
+    ):
+        mock_encoded_event = mocker.Mock(return_value=encoded_event)
+        mocker.patch(
+            "modules.task_queue.tasks.result_event_encoder", mock_encoded_event
+        )
+
+        pass_result_event(mock_encoded_event)
+
+        # assertions
+        mock_redis_in_tasks.publish.assert_called_once_with(
+            channel=PUBSUB_RESULTS_CHANNEL_NAME, message=encoded_event
+        )
+        assert mock_task_queue_logger_in_tasks.debug.call_count == 2
+        assert (
+            mock.call(f"START::{mock_encoded_event.id}::{self.task_name}")
+            in mock_task_queue_logger_in_tasks.debug.call_args_list
+        )
+        assert (
+            mock.call(f"PUBLISHED::{mock_encoded_event.id}")
+            in mock_task_queue_logger_in_tasks.debug.call_args_list
+        )
+
+    @pytest.mark.parametrize(
+        "encoded_event",
+        ["encoded_event_1", "encoded_event_2"],
+    )
+    def test_pass_result_event_fail(
+        self,
+        encoded_event,
+        mocker,
+        mock_task_queue_logger_in_tasks,
+        mock_redis_in_tasks,
+    ):
+        mock_encoded_event = mocker.Mock(side_effect=Exception)
+        mocker.patch(
+            "modules.task_queue.tasks.result_event_encoder", mock_encoded_event
+        )
+
+        # assertions
+        with pytest.raises(Exception):
+            pass_result_event(mock_encoded_event)
+
+        assert mock_task_queue_logger_in_tasks.error.call_count == 1
