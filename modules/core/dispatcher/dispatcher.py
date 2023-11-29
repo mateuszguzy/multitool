ac2403308d99209b55dcaa22290475a837a9b34b
Mateusz Guzy <mateuszguzy6@gmail.com>
2023-11-29 22:13:16 +0100

    change: dispatcher tasks to regular functions

--- modules/core/dispatcher/dispatcher.py
+++ modules/core/dispatcher/dispatcher.py
@@ -55,9 +55,7 @@ class Dispatcher(AbstractModule):
             ):
                 try:
                     logger.debug(f"START::{event.id}")
-                    module_mapper[event.destination_module.split(".")[-1]].delay(
-                        event=event
-                    )
+                    module_mapper[event.destination_module.split(".")[-1]](event=event)
 
                 except Exception as e:
                     logger.error(f"ERROR::{event.id}::{e}")
@@ -71,7 +69,7 @@ class Dispatcher(AbstractModule):
                 for destination_module in module["pass_results_to"]:
                     try:
                         logger.debug(f"START::{event.id}::{destination_module}")
-                        module_mapper[destination_module].delay(event=event)
+                        module_mapper[destination_module](event=event)
 
                     except Exception as e:
                         logger.error(f"ERROR::{event.id}::{e}")
